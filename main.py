a5795284d3e80a5d5f3344f4965c87559c651f4f
SleepSleepRepeat <d3v.jon@gmail.com>
2023-01-25 13:10:33 +0100

    formating changes

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import json
 
 
 def main():
-    file = "./input/WS_22_23/1.sem_inf-1.pdf"
+    file = "./input/WS_22_23/1.sem_e-technik-4.pdf"
 
     plan = PlanParser(file).parse()
 
-    print(f"Title: {plan.title}")
-    print(f"Semester: {plan.semester}")
-    print(f"Group: {plan.group}")
+    print(f"Studiengang: {plan.title}")
+    print(f"Semester:    {plan.semester}")
+    print(f"Gruppe:      {plan.group}")
 
     json_str = {
         "title": plan.title,
